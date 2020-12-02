d2a2a369d502a63fdf601771551d792ba06de432
Gus-Net Flor <gustavotiseira@gmail.com>
2020-12-02 03:06:25 -0300

    HTML: Limpiando codigo JS: fetch login button

---
+++ server/public/index.js
@@ -0,0 +1,35 @@
+let valueLegajo = document.getElementById('valorLegajo')
+let buttonLogin = document.getElementById('botonLegajo')
+let page1 = document.getElementById('page-1')
+let page2 = document.getElementById('page-2')
+let page3 = document.getElementById('page-3')
+let page4 = document.getElementById('page-4')
+
+/*
+Traer elementos del DOM para insertar datos del backend
+*/
+
+buttonLogin.addEventListener('click', () => {
+  if (valueLegajo.value === '') {
+    alert('Ingresa un valor')
+  } else {
+    fetch('http://127.0.0.1:3000/login', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        legajo: valueLegajo.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.body.name) {
+          alert('Legajo no encontrado')
+        } else {
+          page1.classList.add('ocultar')
+          page2.classList.remove('ocultar')
+          userDate = data
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+})
